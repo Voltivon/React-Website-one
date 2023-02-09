693147a17c9fc804fff6e36f7e98329b226af6ef
Tivon <badboytjy@gmail.com>
2023-02-08 20:11:33 -0800

    1

---
+++ react-website-one/src/App.js
@@ -0,0 +1,28 @@
+import Navbar from "./Navbar";
+import About from "./pages/About";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import { Route, Routes} from "react-router-dom"
+
+function App() {
+  
+  return (
+ <>
+ <Navbar/>
+ <div className="container">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/pricing" element={<Pricing/>} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+  </div>
+ 
+ 
+ </>
+  )
+  }
+  
+ 
+
+
+export default App;
